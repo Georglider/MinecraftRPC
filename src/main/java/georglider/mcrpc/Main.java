d3816e5b84de06a41294aea278fc41f210aba98a
quiu <81157387+TheQuiu@users.noreply.github.com>
2022-07-29 00:17:11 +0300

    Update Main.java

--- src/main/java/georglider/mcrpc/Main.java
+++ src/main/java/georglider/mcrpc/Main.java
@@ -5,7 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import net.minecraft.client.MinecraftClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//penis
 public class Main implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
